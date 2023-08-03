f5f91f1f3ba0132f58583879115c6a6faba592de
Kakshil Patel <98046307+Kpat14@users.noreply.github.com>
2023-08-03 01:06:52 -0400

    Update Utils.java

--- project/src/main/java/ca/yorku/eecs/Utils.java
+++ project/src/main/java/ca/yorku/eecs/Utils.java
@@ -17,7 +17,76 @@ import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
 import com.sun.net.httpserver.HttpExchange;
 
-class Utils {        
+class Utils {   
+    //testing communication with api
+    static int PORT = 8080;
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
+        server.createContext("/api/v1/addActor", new AddActorHandler());
+        server.start();
+        System.out.printf("Server started on port %d...\n", PORT);
+    }
+
+    static class AddActorHandler implements HttpHandler {
+        private final Driver driver;
+
+        public AddActorHandler() {
+            // Neo4j database connection parameters
+            String uri = "bolt://localhost:7687"; // Replace with your Neo4j URI
+            String username = "neo4j"; // The default username for Neo4j is 'neo4j'
+            String password = "12345678"; // Replace with your Neo4j password
+
+            // Initialize the Neo4j driver
+            driver = GraphDatabase.driver(uri, AuthTokens.basic(username, password));
+        }
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String requestMethod = exchange.getRequestMethod();
+
+            if ("PUT".equalsIgnoreCase(requestMethod)) {
+                String requestBody = Utils.getBody(exchange);
+
+                String name = null;
+                String actorId = null;
+                try {
+                    JSONObject json = new JSONObject(requestBody);
+                    name = json.getString("name");
+                    actorId = json.getString("actorId");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                if (name != null && !name.isEmpty() && actorId != null && !actorId.isEmpty()) {
+                    createActorNode(name, actorId);
+                    exchange.sendResponseHeaders(200, 0);
+                } else {
+                    exchange.sendResponseHeaders(400, 0);
+                }
+            } else {
+                exchange.sendResponseHeaders(405, 0);
+            }
+
+            OutputStream outputStream = exchange.getResponseBody();
+            outputStream.close();
+        }
+
+        // Method to create an actor node in the database
+        private void createActorNode(String name, String actorId) {
+            String createQuery = "CREATE (a:Actor {name: $name, actorId: $actorId})";
+
+            try (Session session = driver.session()) {
+                session.run(createQuery, 
+                    org.neo4j.driver.Values.parameters("name", name, "actorId", actorId));
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+/////
+    
     // use for extracting query params
     public static Map<String, String> splitQuery(String query) throws UnsupportedEncodingException {
         Map<String, String> query_pairs = new LinkedHashMap<String, String>();
