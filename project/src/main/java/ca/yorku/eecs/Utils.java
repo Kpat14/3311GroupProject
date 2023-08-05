d10350eba9202f6248dbba52602dc66cc5d030d4
Elay Binyamin <72104388+eb2514@users.noreply.github.com>
2023-08-05 15:14:00 -0400

    Update Utils.java
    made all 3 puts

--- project/src/main/java/ca/yorku/eecs/Utils.java
+++ project/src/main/java/ca/yorku/eecs/Utils.java
@@ -11,82 +11,17 @@ import java.net.URLDecoder;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
-import org.neo4j.driver.v1.AuthTokens;
-import org.neo4j.driver.v1.Config;
-import org.neo4j.driver.v1.Driver;
-import org.neo4j.driver.v1.GraphDatabase;
+
+import org.neo4j.driver.v1.*;
+import org.json.*;
 import com.sun.net.httpserver.HttpExchange;
 
-class Utils {   
-    //testing communication with api
-    static int PORT = 8080;
-
-    public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
-        server.createContext("/api/v1/addActor", new AddActorHandler());
-        server.start();
-        System.out.printf("Server started on port %d...\n", PORT);
-    }
-
-    static class AddActorHandler implements HttpHandler {
-        private final Driver driver;
-
-        public AddActorHandler() {
-            // Neo4j database connection parameters
-            String uri = "bolt://localhost:7687"; // Replace with your Neo4j URI
-            String username = "neo4j"; // The default username for Neo4j is 'neo4j'
-            String password = "12345678"; // Replace with your Neo4j password
-
-            // Initialize the Neo4j driver
-            driver = GraphDatabase.driver(uri, AuthTokens.basic(username, password));
-        }
-
-        @Override
-        public void handle(HttpExchange exchange) throws IOException {
-            String requestMethod = exchange.getRequestMethod();
-
-            if ("PUT".equalsIgnoreCase(requestMethod)) {
-                String requestBody = Utils.getBody(exchange);
-
-                String name = null;
-                String actorId = null;
-                try {
-                    JSONObject json = new JSONObject(requestBody);
-                    name = json.getString("name");
-                    actorId = json.getString("actorId");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                if (name != null && !name.isEmpty() && actorId != null && !actorId.isEmpty()) {
-                    createActorNode(name, actorId);
-                    exchange.sendResponseHeaders(200, 0);
-                } else {
-                    exchange.sendResponseHeaders(400, 0);
-                }
-            } else {
-                exchange.sendResponseHeaders(405, 0);
-            }
-
-            OutputStream outputStream = exchange.getResponseBody();
-            outputStream.close();
-        }
-
-        // Method to create an actor node in the database
-        private void createActorNode(String name, String actorId) {
-            String createQuery = "CREATE (a:Actor {name: $name, actorId: $actorId})";
-
-            try (Session session = driver.session()) {
-                session.run(createQuery, 
-                    org.neo4j.driver.Values.parameters("name", name, "actorId", actorId));
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-/////
-    
+class Utils {       
+	
+	private static final String NEO4J_URI = "http://localhost:7687";
+    private static final String NEO4J_USERNAME = "neo4j";
+    private static final String NEO4J_PASSWORD = "12345678";
+	
     // use for extracting query params
     public static Map<String, String> splitQuery(String query) throws UnsupportedEncodingException {
         Map<String, String> query_pairs = new LinkedHashMap<String, String>();
@@ -125,37 +60,47 @@ class Utils {
     
     public void handle(HttpExchange request) throws IOException {
 		// TODO Auto-generated method stub
-		
-        try {
-            if (request.getRequestMethod().equals("addActor")) {
-                //addActor(request);
-                System.out.println("PUT");
-            } else if(request.getRequestMethod().equals("addMovie")) {
-            	System.out.println("PUT");
-            	//addMovie(request);
-            } else if(request.getRequestMethod().equals("addRelationship")) {
-            	System.out.println("PUT");
-            	//addRelationship(request);				
-            } else if(request.getRequestMethod().equals("getActor")) {
-            	System.out.println("GET");
-            	//getActor(request);		
-            }
-            else if(request.getRequestMethod().equals("getMovie")) {
-            	System.out.println("GET");
-            	//getMovie(request);		
-            }
-            else if(request.getRequestMethod().equals("hasRelationship")) {
-            	System.out.println("GET");
-            	//hasRelationship(request);		
-            }
-            else if(request.getRequestMethod().equals("computeBaconNumber")) {
-            	System.out.println("GET");
-            	//computeBacon(request);		
-            }
-            else {
+    	Driver driver = GraphDatabase.driver(NEO4J_URI , AuthTokens.basic(NEO4J_USERNAME, NEO4J_PASSWORD));
+        String requestMethod = request.getRequestMethod();
+        String requestPath = request.getRequestURI().getPath();
+
+        try(Session session = driver.session()) {
+        	if(requestMethod.equals("PUT")) {
+        		if (requestPath.equals("addActor")) {
+                    //addActor(request);
+                    System.out.println("PUT");
+                } else if(requestPath.equals("addMovie")) {
+                	System.out.println("PUT");
+                	addMovie(request, session);
+                } else if(requestPath.equals("addRelationship")) {
+                	System.out.println("PUT");
+                	//addRelationship(request);				
+                }
+        	}
+        	if(requestMethod.equals("GET")) {
+        		 if(requestPath.equals("getActor")) {
+                 	System.out.println("GET");
+                 	//getActor(request);		
+                 }
+                 else if(requestPath.equals("getMovie")) {
+                 	System.out.println("GET");
+                 	//getMovie(request);		
+                 }
+                 else if(requestPath.equals("hasRelationship")) {
+                 	System.out.println("GET");
+                 	//hasRelationship(request);		
+                 }
+                 else if(requestPath.equals("computeBaconNumber")) {
+                 	System.out.println("GET");
+                 	//computeBacon(request);		
+                 }
+        	}
+        	else {
             	System.out.println("Unrecognized command");
             	//sendResponse(request, "Unimplemented method\n", 501);
-            }
+                
+                }
+            
         } catch (Exception e) {
         	e.printStackTrace();
         	//sendResponse(request, "Server error\n", 500);
@@ -163,13 +108,6 @@ class Utils {
         }
 		
 	}
-    
-//    private void sendResponse(HttpExchange request, String data, int restCode) throws IOException {
-//		request.sendResponseHeaders(restCode, data.length());
-//        OutputStream os = request.getResponseBody();
-//        os.write(data.getBytes());
-//        os.close();
-//	}
     
     private void handleGet(HttpExchange request) throws IOException {
     	
@@ -189,17 +127,129 @@ class Utils {
         String response = Long.toString(answer) + "\n";
         //sendResponse(request, response, 200);
     }
-//  private void addMovie(HttpExchange request) throws IOException {
-//	
-//}
     
-//  private void addActor(HttpExchange request) throws IOException {
-//	
-//}
+    @SuppressWarnings("restriction")
+	private void sendResponseCode(HttpExchange request, int code) throws IOException {
+        String responseMessage = null;
+        switch(code) {
+        	case 200:
+        		responseMessage = "OK";
+        		break;
+        	case 400:
+        		responseMessage = "BAD REQUEST";
+        		break;
+        	case 500:
+        		responseMessage = "INTERNAL SERVER ERROR";
+        		break;
+        	default:
+        		responseMessage = "NEW ERROR DETECTED";
+        		break;
+        }
+        request.sendResponseHeaders(code, responseMessage.length());
+        OutputStream os = request.getResponseBody();
+        os.write(responseMessage.getBytes());
+        os.close();
+	}
     
-//  private void addRelationship(HttpExchange request) throws IOException {
-//	
-//}
+ private void addActor(HttpExchange request, Session session) throws IOException {
+    	
+    	String createQuery = "CREATE (a:Actor {name:$name, actorId: $actorId }";
+    	
+    	Map<String, String> queryParam = splitQuery(getBody(request));
+    	
+    	String name = null, actorId = null;
+		name = queryParam.get("name");
+		actorId = queryParam.get("actorId");
+		
+		if (name == null || name.isEmpty() || actorId == null || actorId.isEmpty()) {
+			sendResponseCode(request, 400);
+		}
+		else {
+			String checkQuery = "MATCH (a:Actor {actorId: $actorId}) RETURN a";
+			StatementResult result = session.run(checkQuery, Values.parameters("actorId", actorId));
+			if(result != null) {
+				sendResponseCode(request, 400);
+			}
+			else {
+				try {
+					session.run(createQuery, Values.parameters("name", name, "actorId", actorId));
+					sendResponseCode(request, 200);
+				}
+				catch(Exception e) {
+					sendResponseCode(request, 500);
+				}
+				
+			}
+		}
+    }
+    
+    private void addMovie(HttpExchange request, Session session) throws IOException {
+    	
+    	String createQuery = "CREATE (m:Movie {name:$name, movieId: $movieId }";
+    	
+    	Map<String, String> queryParam = splitQuery(getBody(request));
+    	
+    	String name = null, movieId = null;
+		name = queryParam.get("name");
+		movieId = queryParam.get("movieId");
+		
+		if (name == null || name.isEmpty() || movieId == null || movieId.isEmpty()) {
+			sendResponseCode(request, 400);
+		}
+		else {
+			String checkQuery = "MATCH (m:Movie {movieId: $movieId}) RETURN m";
+			StatementResult result = session.run(checkQuery, Values.parameters("movieId", movieId));
+			if(result != null) {
+				sendResponseCode(request, 400);
+			}
+			else {
+				try {
+					session.run(createQuery, Values.parameters("name", name, "movieId", movieId));
+					sendResponseCode(request, 200);
+				}
+				catch(Exception e) {
+					sendResponseCode(request, 500);
+				}
+				
+			}
+		}
+    }
+    
+    private void addRelationship(HttpExchange request, Session session) throws IOException {
+    	
+    	String relationQuery = "CREATE (MATCH a:Actor, m:Movie "
+    			+ "WHERE a.actorId = '$actorId' AND m.movieId = '$movieId'"
+    			+ "CREATE (a)-[r:ACTED_IN]->m"
+    			+ "RETURN type(r)";
+    	
+    	Map<String, String> queryParam = splitQuery(getBody(request));
+    	
+    	String actorId = null, movieId = null;
+		actorId = queryParam.get("actorId");
+		movieId = queryParam.get("movieId");
+		
+		if (actorId == null || actorId.isEmpty() || movieId == null || movieId.isEmpty()) {
+			sendResponseCode(request, 400);
+		}
+		else {
+			String checkQuery = "MATCH (a:Actor {actorId:$actorId})-[r:ACTED_IN]->(m:Movie {movieId:$movieId})"
+					+ "RETURN COUNT(r)";
+			StatementResult result = session.run(checkQuery, Values.parameters("movieId", movieId));
+			if(result != null) {
+				sendResponseCode(request, 400);
+			}
+			else {
+				try {
+					session.run(relationQuery, Values.parameters("name", actorId, "movieId", movieId));
+					sendResponseCode(request, 200);
+				}
+				catch(Exception e) {
+					sendResponseCode(request, 500);
+				}
+				
+			}
+		}
+    }
     
 //  private void getMovie(HttpExchange request) throws IOException {
 //	
