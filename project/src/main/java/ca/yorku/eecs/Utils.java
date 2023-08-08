4e0787fb8822cd569d72b634dcd3349f7ffe32cc
Kakshil Patel <98046307+Kpat14@users.noreply.github.com>
2023-08-08 19:49:51 -0400

    Update Utils.java

--- project/src/main/java/ca/yorku/eecs/Utils.java
+++ project/src/main/java/ca/yorku/eecs/Utils.java
@@ -1,104 +1,154 @@
 package ca.yorku.eecs;
 
+
+import org.neo4j.driver.v1.*;
+
+import static org.neo4j.driver.v1.Values.parameters;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.stream.Collectors;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
-import java.net.URI;
 import java.net.URLDecoder;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import org.neo4j.driver.v1.AuthTokens;
-import org.neo4j.driver.v1.Config;
-import org.neo4j.driver.v1.Driver;
-import org.neo4j.driver.v1.GraphDatabase;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.neo4j.driver.v1.*;
+
 import com.sun.net.httpserver.HttpExchange;
 
-class Utils {   
-    //testing communication with api
-    static int PORT = 8080;
+public class Utils {
+    private static Driver driver;
 
-    public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
-        server.createContext("/api/v1/addActor", new AddActorHandler());
-        server.start();
-        System.out.printf("Server started on port %d...\n", PORT);
+    public Utils(String uri, String user, String password) {
+        String uriDb = "bolt://localhost:7687";
+        Config config = Config.builder().withoutEncryption().build();
+        driver = GraphDatabase.driver(uriDb, AuthTokens.basic("neo4j", "12345678"), config);
     }
 
-    static class AddActorHandler implements HttpHandler {
-        private final Driver driver;
+    public void addPerson(String name) {
+        try (Session session = driver.session()) {
+            session.writeTransaction(tx -> tx.run("MERGE (a:Person {name: $x})", parameters("x", name)));
+        }
+    }
 
-        public AddActorHandler() {
-            // Neo4j database connection parameters
-            String uri = "bolt://localhost:7687"; // Replace with your Neo4j URI
-            String username = "neo4j"; // The default username for Neo4j is 'neo4j'
-            String password = "12345678"; // Replace with your Neo4j password
+    public static void close() {
+        driver.close();
+    }
+    
+    public void addActor(HttpExchange request, Session session) throws IOException {
+        String addActorQuery = "CREATE (a:Actor {name: $name, actorId: $actorId})";
 
-            // Initialize the Neo4j driver
-            driver = GraphDatabase.driver(uri, AuthTokens.basic(username, password));
+        try (InputStream inputStream = request.getRequestBody()) {
+            // Parse the JSON data from the request body
+            JSONObject jsonObject = new JSONObject(convert(inputStream));
+
+            String name = jsonObject.optString("name");
+            String actorId = jsonObject.optString("actorId");
+
+            if (name == null || name.isEmpty() || actorId == null || actorId.isEmpty()) {
+                sendResponseCode(request, 400);
+            } else {
+            	String checkQuery = "MATCH (a:Actor {actorId: $actorId}) RETURN a";
+    			StatementResult result = session.run(checkQuery, Values.parameters("actorId", actorId));
+                if (result != null && result.hasNext()) {
+                    sendResponseCode(request, 400);
+                } else {
+                    try {
+                        session.run(addActorQuery, Values.parameters("name", name, "actorId", actorId));
+                        sendResponseCode(request, 200);
+                    } catch (Exception e) {
+                        sendResponseCode(request, 500);
+                    }
+
+                }
+            }
+        } catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    }
+    
+    static class AddPersonHandler implements HttpHandler {
+        private Utils utils;
+
+        public AddPersonHandler(Utils utils) {
+            this.utils = utils;
         }
 
         @Override
         public void handle(HttpExchange exchange) throws IOException {
-            String requestMethod = exchange.getRequestMethod();
+            if ("PUT".equals(exchange.getRequestMethod())) {
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(exchange.getRequestBody()))) {
+                    String requestBody = reader.lines().collect(Collectors.joining());
+                    String name = requestBody.trim();
 
-            if ("PUT".equalsIgnoreCase(requestMethod)) {
-                String requestBody = Utils.getBody(exchange);
+                    utils.addPerson(name);
 
-                String name = null;
-                String actorId = null;
-                try {
-                    JSONObject json = new JSONObject(requestBody);
-                    name = json.getString("name");
-                    actorId = json.getString("actorId");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                if (name != null && !name.isEmpty() && actorId != null && !actorId.isEmpty()) {
-                    createActorNode(name, actorId);
-                    exchange.sendResponseHeaders(200, 0);
-                } else {
-                    exchange.sendResponseHeaders(400, 0);
+                    String response = "Person added: " + name;
+                    exchange.sendResponseHeaders(200, response.length());
+                    try (OutputStream os = exchange.getResponseBody()) {
+                        os.write(response.getBytes());
+                    }
                 }
             } else {
-                exchange.sendResponseHeaders(405, 0);
-            }
-
-            OutputStream outputStream = exchange.getResponseBody();
-            outputStream.close();
-        }
-
-        // Method to create an actor node in the database
-        private void createActorNode(String name, String actorId) {
-            String createQuery = "CREATE (a:Actor {name: $name, actorId: $actorId})";
-
-            try (Session session = driver.session()) {
-                session.run(createQuery, 
-                    org.neo4j.driver.Values.parameters("name", name, "actorId", actorId));
-
-            } catch (Exception e) {
-                e.printStackTrace();
+                exchange.sendResponseHeaders(405, 0); // Method not allowed
             }
         }
     }
-/////
+
     
-    // use for extracting query params
+        
+     
+
+
+	
+
+
+
+
+	// use for extracting query params
     public static Map<String, String> splitQuery(String query) throws UnsupportedEncodingException {
-        Map<String, String> query_pairs = new LinkedHashMap<String, String>();
+        Map<String, String> query_pairs = new LinkedHashMap<>();
+        if (query == null || query.isEmpty()) {
+            return query_pairs;
+        }
+
         String[] pairs = query.split("&");
         for (String pair : pairs) {
             int idx = pair.indexOf("=");
-            query_pairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"), URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
+            if (idx > 0 && idx < pair.length() - 1) {
+                query_pairs.put(
+                    URLDecoder.decode(pair.substring(0, idx), "UTF-8"),
+                    URLDecoder.decode(pair.substring(idx + 1), "UTF-8")
+                );
+            }
         }
         return query_pairs;
     }
 
-    // one possible option for extracting JSON body as String
+
+    
+
+
+
+	// one possible option for extracting JSON body as String
     public static String convert(InputStream inputStream) throws IOException {
                 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(inputStream))) {
@@ -123,99 +173,28 @@ class Utils {
         return buf.toString();
         }
     
-    public void handle(HttpExchange request) throws IOException {
-		// TODO Auto-generated method stub
-		
-        try {
-            if (request.getRequestMethod().equals("addActor")) {
-                //addActor(request);
-                System.out.println("PUT");
-            } else if(request.getRequestMethod().equals("addMovie")) {
-            	System.out.println("PUT");
-            	//addMovie(request);
-            } else if(request.getRequestMethod().equals("addRelationship")) {
-            	System.out.println("PUT");
-            	//addRelationship(request);				
-            } else if(request.getRequestMethod().equals("getActor")) {
-            	System.out.println("GET");
-            	//getActor(request);		
-            }
-            else if(request.getRequestMethod().equals("getMovie")) {
-            	System.out.println("GET");
-            	//getMovie(request);		
-            }
-            else if(request.getRequestMethod().equals("hasRelationship")) {
-            	System.out.println("GET");
-            	//hasRelationship(request);		
-            }
-            else if(request.getRequestMethod().equals("computeBaconNumber")) {
-            	System.out.println("GET");
-            	//computeBacon(request);		
-            }
-            else {
-            	System.out.println("Unrecognized command");
-            	//sendResponse(request, "Unimplemented method\n", 501);
-            }
-        } catch (Exception e) {
-        	e.printStackTrace();
-        	//sendResponse(request, "Server error\n", 500);
-        	System.out.println("Server error");
+    public static void sendResponseCode(HttpExchange request, int code) throws IOException {
+        String responseMessage = null;
+        switch (code) {
+            case 200:
+                responseMessage = "200 OK";
+                break;
+            case 400:
+                responseMessage = "400 BAD REQUEST";
+                break;
+            case 404:
+                responseMessage = "404 DOES NOT EXIST";
+                break;
+            case 500:
+                responseMessage = "500 INTERNAL SERVER ERROR";
+                break;
+            default:
+                responseMessage = "NEW ERROR DETECTED";
+                break;
         }
-		
-	}
-    
-//    private void sendResponse(HttpExchange request, String data, int restCode) throws IOException {
-//		request.sendResponseHeaders(restCode, data.length());
-//        OutputStream os = request.getResponseBody();
-//        os.write(data.getBytes());
-//        os.close();
-//	}
-    
-    private void handleGet(HttpExchange request) throws IOException {
-    	
-        URI uri = request.getRequestURI();
-        String query = uri.getQuery();
-        System.out.println(query);
-        Map<String, String> queryParam = splitQuery(query);
-        System.out.println(queryParam);
-        long first = Long.parseLong(queryParam.get("firstNumber"));
-        long second = Long.parseLong(queryParam.get("secondNumber"));
-        
-        // add code for incorrect parameters
-
-        /* TODO: Implement the math logic */
-        long answer = first + second;
-        System.out.println(first+","+second+","+answer);
-        String response = Long.toString(answer) + "\n";
-        //sendResponse(request, response, 200);
+        request.sendResponseHeaders(code, responseMessage.length());
+        OutputStream os = request.getResponseBody();
+        os.write(responseMessage.getBytes());
+        os.close();
     }
-//  private void addMovie(HttpExchange request) throws IOException {
-//	
-//}
-    
-//  private void addActor(HttpExchange request) throws IOException {
-//	
-//}
-    
-//  private void addRelationship(HttpExchange request) throws IOException {
-//	
-//}
-    
-//  private void getMovie(HttpExchange request) throws IOException {
-//	
-//}
-    
-//  private void getActor(HttpExchange request) throws IOException {
-//	
-//}
-    
-//  private void hasRelationship(HttpExchange request) throws IOException {
-//	
-//}
-    
-//  private void computeBaconNumber(HttpExchange request) throws IOException {
-//	
-//}
-
-    
 }
