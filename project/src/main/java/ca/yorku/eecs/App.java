2f131c50413e02d2e4837c1391126e6122e44826
Kakshil Patel <98046307+Kpat14@users.noreply.github.com>
2023-08-09 13:10:29 -0400

    Update App.java

--- project/src/main/java/ca/yorku/eecs/App.java
+++ project/src/main/java/ca/yorku/eecs/App.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
 
-import ca.yorku.eecs.Utils.AddPersonHandler;
-
 public class App 
 {
     static int PORT = 8080;
@@ -13,9 +11,9 @@ public class App
     {
         HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", PORT), 0);
         // TODO: two lines of code are expected to be added here
-        Utils adder = new Utils("http://localhost:7474/2", "neo4j", "12345678");
-        server.createContext("/addPerson", new AddPersonHandler(adder));
         // please refer to the HTML server example 
+        Utils adder = new Utils();
+	    server.createContext("/api/v1/", adder::handle);
         server.start();
         System.out.printf("Server started on port %d...\n", PORT);
     }
